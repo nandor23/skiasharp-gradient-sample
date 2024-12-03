d83d72b3983b6ea53d54ae6977de617ab3378b21
Nandor Krizbai <krizbai.nandor@gmail.com>
2024-12-03 06:13:24 +0100

    Initial commit

---
+++ SkiaSharp.Gradient/Program.cs
@@ -0,0 +1,42 @@
+﻿using SkiaSharp;
+
+const float size = 1000;
+
+LinearGradient();
+RadialGradient();
+
+void LinearGradient()
+{
+    using var fileStream = new FileStream("linear_gradient.svg", FileMode.Create);
+    using var canvas = SKSvgCanvas.Create(new SKRect(0f, 0f, size, size), fileStream);
+    using var paint = new SKPaint
+    {
+        Shader = SKShader.CreateLinearGradient(
+            new SKPoint(0, 0),
+            new SKPoint(size, size),
+            [SKColors.CornflowerBlue, SKColors.YellowGreen],
+            null,
+            SKShaderTileMode.Clamp
+        )
+    };
+    canvas.DrawPaint(paint);
+    canvas.Flush();
+}
+
+void RadialGradient()
+{
+    using var fileStream = new FileStream("radial_gradient.svg", FileMode.Create);
+    using var canvas = SKSvgCanvas.Create(new SKRect(0f, 0f, size, size), fileStream);
+    using var paint = new SKPaint
+    {
+        Shader = SKShader.CreateRadialGradient(
+            new SKPoint(size / 2, size / 2),
+            size / 2,
+            [SKColors.CornflowerBlue, SKColors.YellowGreen],
+            null,
+            SKShaderTileMode.Clamp
+        )
+    };
+    canvas.DrawPaint(paint);
+    canvas.Flush();
+}
